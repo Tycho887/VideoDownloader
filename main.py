b0c1e1a3654675c0eb91566656c0c34979133587
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2024-06-22 14:09:36 +0200

    download handler tar nå også youtu.be-lenker. flere typer nedlastingsformater har blitt lagt til i _youtube

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from scripts import download_video
+from scripts._youtube import vidformats, audformats
 
 if __name__ == "__main__":
     url = input("Enter the video URL to download: ")
-    file = download_video(url)
-    print(f"Downloaded video saved as {file}")
+    format = input(f"Choose format video/audio (supported formats: {vidformats} and {audformats}): ")
+    if format in vidformats:
+        print('Downloading video...')
+        file = download_video(url, format)
+        print(f"Downloaded video saved as {file}")
+    elif format in audformats:
+        print('Downloading audio...')
+        file = download_video(url, format)
+        print(f"Downloaded video saved as {file}")
+    else:
+        print("The format you entered is not a supported format. Try again")
--- scripts/_youtube.py
+++ scripts/_youtube.py
@@ -1,22 +1,46 @@
+from __future__ import unicode_literals
 import yt_dlp
 import os
 import sys
 
 # Add the 
 
-def download_youtube_video(url, video_destination):
+vidformats = ['mp4', 'mkv', 'mpeg']
+audformats = ['wav', 'mp3']
+
+def download_youtube_video(url, format, video_destination):
     # Define the options for yt-dlp
-    ydl_opts = {
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'format': 'bestvideo+bestaudio/best',
-        'merge_output_format': 'mp4',
-    }
+    if format in vidformats:
+        ydl_opts = {
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'format': 'bestvideo+bestaudio/best',
+            #'merge_output_format': f'{format}',
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': f'{format}',
+            }]
+        }
+    elif format in audformats:
+        ydl_opts = {
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': f'{format}',
+                'preferredquality': '192',
+            }],
+            'postprocessor_args': [
+                '-ar', '48000'
+            ],
+            'prefer_ffmpeg': True,
+            'keepvideo': False
+        }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             video_title = info_dict.get('title', None)
-            video_ext = info_dict.get('ext', 'mp4')  # Default to mp4 if no ext found
+            video_ext = info_dict.get('ext', f'{format}')  # Default to mp4 if no ext found
             video_filename = f'{video_destination}/{video_title}.{video_ext}'
 
         print(f"Downloaded: {video_filename}")
