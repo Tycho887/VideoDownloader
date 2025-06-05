629665f08ba5a1a3584274926678d7df33c6cb5d
michael <michael.ruben.johansen@nmbu.no>
2025-06-05 21:56:41 +0200

    added changes

--- depr/lib/__init__.py
+++
@@ -1,6 +0,0 @@
-from .utils.get_env_variables import get_bot_token
-from .utils.utility import *
-from .downloaders.youtube import youtube_downloader as download_youtube_video
-from .downloaders.twitter import download_twitter_video
-from .utils.validation import *
-from .utils.constants import *
